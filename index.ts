fa4d3c490ff88f50a8f697f063c097a88731aaa1
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2025-01-09 17:30:59 +0600

    [HW-83] Implement user registration and login routes

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
+import usersRouter from "./routers/users";
 
 
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 const run = async () => {
     await mongoose.connect('mongodb://localhost/musicApp');
---
+++ types.d.ts
@@ -0,0 +1,5 @@
+export interface UserField {
+    username: string;
+    password: string;
+    token: string;
+}
